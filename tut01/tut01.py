e1b472ae0127dbf42a5227b418f47ad0732bcbe7
Gargi Chandrakar <74736711+gargi2718@users.noreply.github.com>
2022-08-27 20:46:41 +0530

    Added tut 1 solutions

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,13 @@
-def factorial(int x):
-    
+def factorial(x):
+    if(x<0):
+        return "undefined"
+            
+    elif(x==0 or x==1):
+        return 1
+    else:
+        return x*factorial(x-1)
 
+    
 x=int(input("Enter the number whose factorial is to be found"))
 factorial(x)
+print(f"The factorial of {x} is {factorial(x)}")
