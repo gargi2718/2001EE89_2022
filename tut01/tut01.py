f0dd98a6d3d8990d43918ee58236044f61d0f244
Gargi Chandrakar <74736711+gargi2718@users.noreply.github.com>
2022-09-14 09:35:51 +0530

    Added questions

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,13 +1,14 @@
-def factorial(x):
-    if(x<0):
-        return "undefined"
-            
-    elif(x==0 or x==1):
-        return 1
-    else:
-        return x*factorial(x-1)
+def octact_identification(mod=5000):
+###Code
 
-    
-x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
-print(f"The factorial of {x} is {factorial(x)}")
+
+from platform import python_version
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+mod=5000
+octact_identification(mod)
