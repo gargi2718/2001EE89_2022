5a633354d35c752a10dce47d180de9869758b5d7
Gargi Chandrakar <74736711+gargi2718@users.noreply.github.com>
2022-10-11 23:54:43 +0530

    Codes added for part 1 2 3

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -2,7 +2,9 @@ from platform import python_version
 
 import openpyxl
 import pandas as pd
+import numpy as np
 
+#For part 1 i.e.  Data Pre-processing : Code same as that of tut 1 except handling excel file
 #To identify the octant by using U',V',W' values in the dataframe
 def octant(df):
     for index, row in df.iterrows():
@@ -32,17 +34,15 @@ def octant(df):
     return df
 
 #To calculate the no of rows(in a given range from start to stop) having a particular octant value
-
-
 def no_of_counts(df, start, stop, octant_value):
     ci = 0  # counter variable
-    for j in range(start, stop, 1):
+    for j in range(start, stop+1, 1):
         if(df.loc[j, "Octant"] == octant_value):
             ci = ci+1
 
     return ci
 
-
+# Part 2 : Calculation of octant count in a given mod range
 def octant_identification(mod=5000):
     try:
         df = pd.read_excel(
@@ -62,8 +62,8 @@ def octant_identification(mod=5000):
     df['Octant'] = df["U"]  # declaring Octant with dummy variables for now
     # for calculating the octant and storing it in Octant column of the dataframe
     df = octant(df)
-    df[''] = ''  # adding an empty column with empty values
-    df.at[1, ''] = "User Input"  # adding it the way it was in output file
+    df[' '] = ''  # adding an empty column with empty values
+    df.at[1, ' '] = "User Input"  # adding it the way it was in output file
     df['Octant ID'] = ''  # creating an empty column for storing ranges
 
     df.at[1, 'Octant ID'] = 'MOD '+str(mod)
@@ -105,16 +105,109 @@ def octant_identification(mod=5000):
             end = n-1
     try:
         df.to_excel(
-            'C:/Users/Gargi/Desktop/2001EE89_2022/tut02/octant_output.xlsx', index=None)
+            'C:/Users/Gargi/Desktop/2001EE89_2022/tut02/output_octant_transition_identify.xlsx', index=False)
     except:
         print("Permission denied")
 
 
-# calling function
+# calling function for Part 1 and Part 2
 octant_identification()
+
+
+#Part 3: Calculating the transition count for 
+def overall_transition_count():
+    k = [1, -1, 2, -2, 3, -3, 4, -4]
+    #counter to keep track of rows so that we can store our output in specific rows 
+    crr = 10
+    # for reading the file and handling exception for wrong file path
+    try:
+        df = pd.read_excel(
+            r'C:\Users\Gargi\Desktop\2001EE89_2022\tut02\output_octant_transition_identify.xlsx')
+    except ImportError:
+        print("Wrong File Path. Please check path and try again!")
+
+    n = len(df)
+    #creating 3 columns for 
+    df.at[crr, "Octant ID"] = "Overall Transition Count"
+    df.at[crr+1,"Octant ID"]="To"
+    df.at[crr+3, " "] = "From"
+    for i in range(8):
+        df.at[crr+2, k[i]] = k[i]
+    for i in range(8):
+        df.at[crr+3+k.index(k[i]), "Octant ID"] = k[i]
+    for j in range(8):
+        for i in range(8):
+            df.at[crr+j+3,k[i]]=0
+
+    for i in range(0, n-1):
+        df.at[crr+3+k.index(df.at[i, "Octant"]), df.at[i+1, "Octant"]] +=1
+
+    crr=crr+14
+    try:
+        df.to_excel(
+            'C:/Users/Gargi/Desktop/2001EE89_2022/tut02/output_octant_transition_identify.xlsx', index=False)
+    except:
+        print("Permission denied")
+
+
+def mod_transition_count(mod=5000):
+    try:
+        df = pd.read_excel(
+            r'C:\Users\Gargi\Desktop\2001EE89_2022\tut02\output_octant_transition_identify.xlsx')
+    except ImportError:
+        print("Wrong File Path.Please check path and try again!")
+
+    k = [1, -1, 2, -2, 3, -3, 4, -4]
+    crr = 24
+    n=len(df)
+    begin=0000
+    end=mod-1
+    while(begin<n):
+        # Storing titles for transition count of each index
+        df.at[crr+1,"Octant ID"]=str(begin)+"-"+ str(end)
+        df.at[crr, "Octant ID"] = "Mod Transition Count"
+        df.at[crr+1, 1] = "To"
+        df.at[crr+3, " "] = "From"
+        #Storing column and row titles for mod transition count table 
+        for i in range(8):
+            df.at[crr+2,k[i]]=k[i]
+        for i in range(8):
+            df.at[crr+3+k.index(k[i]), "Octant ID"] = k[i]
+        #Creating cells with null values to store counts later
+        for j in range(8):
+            for i in range(8):
+                df.at[crr+j+3, k[i]] = 0
+        #Counting transitions 
+        for i in range(begin, end):
+            df.at[crr+3+k.index(df.at[i, "Octant"]), df.at[i+1, "Octant"]] += 1
+        crr+=14
+        if(end != n-1):
+            begin = end+1
+        if(end == n-1):
+            break
+        if(begin+mod-1 <= n-1):
+            end = begin+mod-1
+        elif(begin+mod-1 > n-1):
+            end = n-1
+
+    #Writing to output file
+    try:
+        df.to_excel(
+            'C:/Users/Gargi/Desktop/2001EE89_2022/tut02/output_octant_transition_identify.xlsx', index=False)
+    except:
+        print("Permission denied")
+
+#Function calling for part 3
+overall_transition_count()
+mod_transition_count()
+
+
 ver = python_version()
 
 if ver == "3.8.10":
     print("Correct Version Installed")
 else:
     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+
+
